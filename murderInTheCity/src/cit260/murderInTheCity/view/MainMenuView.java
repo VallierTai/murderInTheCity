47a1a770ccc3c0d3c30c039efa47bfb7139f0641
Tai <Tai@Tiger.hsd1.ut.comcast.net>
2016-02-19 11:55:34 -0700

    2/19/16 - Added display for main menu

--- murderInTheCity/src/cit260/murderInTheCity/view/MainMenuView.java
+++ murderInTheCity/src/cit260/murderInTheCity/view/MainMenuView.java
@@ -5,13 +5,120 @@
  */
 package cit260.murderInTheCity.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Brad Bastian
  */
-public class MainMenuView {
+public class MainMenuView
+{ 
+    private String menu;
+    private String promptMessage = "\nPlease enter an option: ";
+    public MainMenuView(){
+        this.menu = "\n"
+                +"\n--------------------------------------"
+                +"\n| Main Menu "
+                + "\nN - Start new game"
+                + "\nG - Get and start saved game"
+                + "\nH - Get help on how to play the game"
+                + "\nS - Save game"
+                + "\nQ - Quit"
+                + "\n======================================";
+     }
+                
+               
 
     void displayMainMenuView() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }   
+        boolean done = false; // set flag to not done
+        do { 
+            // prompt for and get players name
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals("Q"))
+                    // user wants to quit
+                    return; // exit the game
+            // do the requested action and display the next view
+            done = this.doAction(menuOption);
+        } while (!done);
+       
+    }    
+
+    private String getMenuOption() {
+       
+        Scanner keyboard = new Scanner(System.in);
+        String value = "";
+        
+        // set flag to invalid value entered
+        boolean valid = false;
+        // while a valid name has not been retrieved
+        while(!valid) {
+            // display menu
+            System.out.println(this.menu);
+            
+            // prompt for the player's option
+            System.out.println(this.promptMessage);
+            
+            // get the name from the keyboard
+            value = keyboard.nextLine();
+            // trim off the excess blanks
+            value = value.trim();
+            
+            // if the name is invalid (less than one character in length)
+            if (value.length() < 1) {
+                System.out.println("Invalid value - the value can not be blank");
+                // and repeat again
+                continue;
+            }
+            // set flag to end repetition
+            valid = true;
+        }
+        // return the value
+        return value;
+    }  
+        
+
+    public boolean doAction(String choice) {
+        
+       choice = choice.toUpperCase(); // convert choice to upper case
+       
+       switch (choice){
+           case "N": // create and starta new game
+               this.startNewGame();
+               break;
+           case "G": // get and start an exisiting game
+               this.startExistingGame();
+               break;
+           case "H": // display the help menu
+               this.displayHelpMenu();
+               break;
+           case "S": // save the current game
+               this.saveGame();
+               break;
+           default:
+               System.out.println("\n*** Invalid selection *** Try again");
+               break;
+       }              
+        return false;       
+    }           
+
+    private void startNewGame() {
+        System.out.println("*** startNewGame function called ***");
+    }
+
+    private void startExistingGame() {
+        System.out.println("*** startExistingGame function called ***");
+    }
+
+    private void displayHelpMenu() {
+       System.out.println("*** displayHelpMenu function called ***"); 
+    }
+
+    private void saveGame() {
+        System.out.println("*** startExistingGame function called ***");
+        
+    }
+ 
+    
 }
+
+ 
