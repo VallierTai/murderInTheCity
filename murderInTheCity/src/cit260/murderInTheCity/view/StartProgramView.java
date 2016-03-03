db4a70111ae78eb6ef34dd781c1d12ce09c4a9ab
mrbra <mrbra@Brads-PC>
2016-03-02 19:04:57 -0700

    03/02/16: Updated StartProgramView to remove method that is covered by super class.

--- murderInTheCity/src/cit260/murderInTheCity/view/StartProgramView.java
+++ murderInTheCity/src/cit260/murderInTheCity/view/StartProgramView.java
@@ -39,36 +39,6 @@ public class StartProgramView extends View {
             );
     }
 
-    private String getPlayersName() {
-        // keyboard input stream
-        Scanner keyboard = new Scanner(System.in);
-        String value = "";
-        
-        // set flag to invalid value entered
-        boolean valid = false;
-        // while a valid name has not been retrieved
-        while(!valid) {
-            // prompt for the player's name
-            System.out.println(this.promptMessage);
-            
-            // get the name from the keyboard
-            value = keyboard.nextLine();
-            // trim off the excess blanks
-            value = value.trim();
-            
-            // if the name is invalid (less than one character in length)
-            if (value.length() < 1) {
-                System.out.println("Invalid value - the value can not be blank");
-                // and repeat again
-                continue;
-            }
-            // set flag to end repetition
-            valid = true;
-        }
-        // return the value
-        return value;
-    }
-
     @Override
     public boolean doAction(String playersName) {
         if (playersName.length() < 2) {
