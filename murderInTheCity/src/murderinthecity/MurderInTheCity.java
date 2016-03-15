122b0457e0132eda6a16850122edba95f8ef7151
Tai <Tai@10.0.0.12>
2016-03-15 14:41:09 -0600

    03/05/16 - Created try catch for calculate time of death

--- murderInTheCity/src/murderinthecity/MurderInTheCity.java
+++ murderInTheCity/src/murderinthecity/MurderInTheCity.java
@@ -49,7 +49,14 @@ public class MurderInTheCity {
      */
     public static void main(String[] args) {
         StartProgramView startProgramView = new StartProgramView();
+        try {
+            //create StartProgramView and start the program
+        
         startProgramView.display();
-
+        } catch (Throwable te){
+            System.out.println(te.getMessage());
+            te.printStackTrace();
+            startProgramView.display();
+        }
     }
 }
